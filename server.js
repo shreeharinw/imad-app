ce4017bffa2105b6a8e92447e2e9f33946b36bdd
Shreehari N W <34746409+shreeharinw@users.noreply.github.com>
2018-03-03 21:51:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone = {
+    'title':'article 1',
+    'heading':'article one',
+    'date': '01 jan 2018',
+    'content': `
+    <p> Born to a Gujarati family in Vadnagar, Modi helped his father sell tea as a child, and later ran his own stall. </p>`
+};
+
+function createtemplate(data) {
+var title = data.title;
+var heading = data.heading;
+var content = data.content;
+var date = data.date;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+        ${title}
+        </title>
+        <meta name="viewport" content="width-device-width,initial scale=1"/>
+         <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <div class="article_section">
+            <div>
+                <a href = "/">Home</a>
+            </div>            
+            <hr/>
+            <h1>
+               ${heading}
+            </h1>
+            <div>
+               ${date}
+            </div>
+            <div>
+            <p>
+                ${content}
+            </p>
+            </div>
+        </div>
+    </body>
+</html>
+`;
+return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createtemplate(articleone));
 });
 
 app.get('/article-two', function (req, res) {
